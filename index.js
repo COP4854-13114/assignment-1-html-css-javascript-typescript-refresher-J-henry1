9c1b917d25a3dac398c9b169864101a293a79dfd
JARED HENRY <109435235+J-henry1@users.noreply.github.com>
2024-01-25 12:41:44 -0500

    added code to make write review/show review dynmic with buttons

--- index.js
+++ index.js
@@ -4,3 +4,36 @@ function updateImage(imageSource){
     document.getElementById('image').src = imageSource;
     
 }
+
+
+function showReviews(){
+   
+    var reviewHistory =  document.getElementById('reviewHistory');
+    var inputField = document.getElementById('reviewInput')
+
+    if(inputField.style.display === 'block'){
+        
+        reviewHistory.style.display = 'block';
+        inputField.style.display = 'none';
+    }
+    else{
+        reviewHistory.style.display = 'block';
+    }
+
+    
+}// end showReviews function
+
+function writeReview(){
+
+    inputField = document.getElementById('reviewInput');
+    reviewHistory = document.getElementById('reviewHistory');
+
+    if(reviewHistory.style.display === 'block'){
+        reviewHistory.style.display = 'none';
+        inputField.style.display = 'block';
+        
+    }
+    else{
+        inputField.style.display = 'block';
+    }
+}//end writeReview function
